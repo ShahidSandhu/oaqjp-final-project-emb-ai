6cd4ccc5cc60c68f38e0627e030e319a46dcd377
ShahidSandhu <sandhulive@gmail.com>
2024-09-18 17:08:39 -0400

    package created and tested

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -27,33 +27,18 @@ def emotion_detector(text_to_analyse):
         return {'message' : 'Request Time out', 'label': 'none', 'score': 'none'}
 
     # Parsing the JSON response from the API
-    response_in_json = json.loads(response.text)
+    response_json = json.loads(response.text)
 
-    return response_in_json['emotionPredictions'][0]['emotion']
-    
-    
-    
-    
-    
-    
-    
-    emotion_score = {}
-     # If the response status code is 200, extract the score from the response_in_json
-    '''  
+    emotion_score = response_json['emotionPredictions'][0]['emotion']
+
+     # If the response status code is 200, extract the score from the response_in_json 
     if response.status_code == 200:
-        emotion_score = {
-            'anger': response_in_json['anger'],
-            'disgust': response_in_json['disgust_score'],
-            'fear': response_in_json['fear_score'],
-            'joy': response_in_json['joy_score'],
-            'sadness': response_in_json['sadness_score']
-            }
+        emotion_score
     # If the response status code is 500 , set score to None
     else:
         emotion_score = {
             'anger': none, 'disgust': none,
             'fear': none, 'joy': none, 'sadness': none
             }
-    '''
     # Returning a dictionary containing sentiment analysis results
     return emotion_score
